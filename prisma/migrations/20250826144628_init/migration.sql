163a8c4c0ae4d8d495d14d857de0b67235975e32
Eric Murta <ericmurta@hotmail.com>
2025-08-26 11:48:52 -0300

    running migrations

---
+++ prisma/migrations/20250826144628_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "items" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "version" TEXT NOT NULL,
+    "mcVersion" TEXT NOT NULL,
+    "language" TEXT NOT NULL,
+    "downloads" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "image" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'PENDING',
+    "pendingPercentage" INTEGER NOT NULL DEFAULT 0,
+    "badges" TEXT NOT NULL
+);
